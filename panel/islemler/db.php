bf5e1947648ea3ca326bc5c9c2e79d3cb4d4e742
Tolga Demir <40199261+tolgademir@users.noreply.github.com>
2021-07-04 18:23:53 +0300

    Project loaded

---
+++ panel/islemler/db.php
@@ -0,0 +1,37 @@
+<?php
+
+$host="localhost";
+
+$veritabani_ismi="cv";
+
+$kullanici_adi="root";
+
+$sifre="";
+
+try 
+{
+	$db = new PDO("mysql:host=$host; dbname=$veritabani_ismi; charset=utf8", $kullanici_adi,$sifre);
+      
+} 
+catch (PDOException $e) {
+	echo $e->getmessage();
+      
+}
+
+$sorgu=$db->prepare("SELECT * FROM yonetici_paneli");
+$sorgu->execute();
+$yonetici_paneli=$sorgu->fetch(PDO::FETCH_ASSOC);
+
+$sorgu=$db->prepare("SELECT * FROM seo");
+$sorgu->execute();
+$seo=$sorgu->fetch(PDO::FETCH_ASSOC);
+
+$sorgu=$db->prepare("SELECT * FROM ust_kisim");
+$sorgu->execute();
+$ust_kisim=$sorgu->fetch(PDO::FETCH_ASSOC);
+
+$sorgu=$db->prepare("SELECT * FROM sosyal_medya");
+$sorgu->execute();
+$sosyal_medya=$sorgu->fetch(PDO::FETCH_ASSOC);
+
+?>
